ecf67e4e3865425c9dff74f1e4b038a3e6225475
UstubecEmre <emresb1999@gmail.com>
2025-09-23 13:32:36 +0300

    feat(clean_data): implement clean_iris_data() function to encode and extract new features

---
+++ second_week_project/iris-mlops/src/clean_data.py
@@ -0,0 +1,56 @@
+#%% import required libraries
+import os # for operating system operations (like making directory etc...)
+# import numpy as np # for linear algebra, convert to data type
+import pandas as pd # for data manipulation
+# from sklearn.preprocessing import OneHotEncoder
+
+
+#%% define clean_iris_data(input_path, output_path)
+def clean_iris_data(input_path = 'data/raw/iris.csv', output_path = 'data/processed/iris_processed.csv'):
+    """ Cleans, encodes and applies feature engineering to the Iris dataset."""
+    try:
+    # load dataset
+        iris_df = pd.read_csv(input_path)
+    except FileNotFoundError:
+        raise FileNotFoundError(f'Error: The file at {input_path} not found!!!')
+        # return None, None 
+    print("Starting data encoding and feature engineering:)")
+    
+    
+    # make a copy to avoid modifying the original DataFrame
+    iris_cleaned = iris_df.copy()
+    
+    # There are not any missing values
+    
+    # drop 'id' column
+    if 'Id' in iris_cleaned.columns:
+        iris_cleaned = iris_cleaned.drop('Id', axis = 1)
+        print("Dropped 'Id' column...")
+    
+    # apply one hot encoding to the categorical column
+    iris_encoded = pd.get_dummies(iris_cleaned, columns=['species'], drop_first= True)
+    print("Applied OHE to 'species' column...")
+    # use feature extraction techniques
+    
+    iris_encoded['sepal_ratio'] = iris_encoded['sepal_length'] / iris_encoded['sepal_width']
+    iris_encoded['petal_ratio'] = iris_encoded['petal_length'] / iris_encoded['petal_width']
+    print("Created new features => 'sepal_ratio' and 'petal_ratio'")
+    
+    
+    # make output path
+    os.makedirs(os.path.dirname(output_path), exist_ok= True)
+    
+    # save cleaned and encoded dataset
+    iris_encoded.to_csv(output_path, index = False)
+    
+    # show some information about cleaned and encoded dataset
+    print(f"Encoded dataset saved: {output_path}")
+    print(f"Total Missing Values: \nThere are {iris_encoded.isnull().sum().sum()} missing values")
+    
+    features = list(iris_encoded.columns)
+    
+    print(f"Model features: {features}")
+    
+    # return cleaned and encoded dataset and new features
+    return iris_encoded, features
+        
