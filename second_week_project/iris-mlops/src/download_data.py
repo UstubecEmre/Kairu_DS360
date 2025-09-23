0cf7420e7ef4d266bdbb0caf0b1123e5694fca47
UstubecEmre <emresb1999@gmail.com>
2025-09-23 12:25:59 +0300

    feat(second_week_project): add basic project structure

---
+++ second_week_project/iris-mlops/src/download_data.py
@@ -0,0 +1,30 @@
+#%% import required libraries
+import os # for operating system
+import pandas as pd # for data manipulation
+import seaborn as sns # for dataset loading 
+
+#%% define download_iris() function
+def download_iris_data():
+    """Load iris dataset from seaborn"""
+    
+    # create directories
+    os.makedirs('data/raw',exist_ok= True)
+    
+    # load iris data set
+    iris_df = sns.load_dataset('iris')
+    
+    # save the raw data
+    pd.to_csv('data/raw/iris.csv', index = False)
+    
+    # show basic information about iris dataset
+    print("Iris dataset loaded successfully:)")
+    print(f"Iris dataset shape: {iris_df.shape}")
+    print(f"Iris dataset total record: {iris_df.shape[0]}")
+    print(f"Columns of Iris dataset: {list(iris_df.columns)}")
+    print(f"Total Missing Values of Iris Dataset: {iris_df.isnull().sum()}")
+    
+    
+    return iris_df 
+
+
+#%% call the main function
