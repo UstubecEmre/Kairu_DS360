1269a383e3e22aeda1b84665e5ff31ca479a212a
UstubecEmre <emresb1999@gmail.com>
2025-09-23 12:35:07 +0300

    fix(download_dataset): resolve issue with saving iris dataset

--- second_week_project/iris-mlops/src/download_data.py
+++ second_week_project/iris-mlops/src/download_data.py
@@ -14,20 +14,20 @@ def download_iris_data():
     iris_df = sns.load_dataset('iris')
     
     # save the raw data
-    pd.to_csv('data/raw/iris.csv', index = False)
+    iris_df.to_csv('data/raw/iris.csv', index = False)
     
     # show basic information about iris dataset
     print("Iris dataset loaded successfully:)")
     print(f"Iris dataset shape: {iris_df.shape}")
     print(f"Iris dataset total record: {iris_df.shape[0]}")
     print(f"Columns of Iris dataset: {list(iris_df.columns)}")
-    print(f"Total Missing Values of Iris Dataset: {iris_df.isnull().sum()}")
+    print(f"Total Missing Values of Iris Dataset:\n {iris_df.isnull().sum()}")
     
     
     return iris_df 
 
 
 #%% call the download_iris_data on main function
-if __name__== '__main.py__':
+if __name__== '__main__':
     # call the download_iris_data()
     download_iris_data()
