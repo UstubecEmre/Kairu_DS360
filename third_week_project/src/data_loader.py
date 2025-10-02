92e993fd660643d40b9870b71a136dd4ec8a0da5
UstubecEmre <emresb1999@gmail.com>
2025-10-02 14:42:15 +0300

    feat(data_loader): add load_loan_data function with optional disk save

--- third_week_project/src/data_loader.py
+++ third_week_project/src/data_loader.py
@@ -4,9 +4,9 @@ import pandas as pd
 
 
 #%% define load_data() function (load_data() fonksiyonunu tanimla)
-def download_loan_data():
+def load_loan_data(save_to_disk = True):
     """Loan datasetini indirir ve data klasorune kaydeder"""
-    os.makedirs("third_week_project/data/raw",exist_ok = True)
+    
     
     try:
         loan_df = pd.read_csv(r'D:\Datasets\loan_dataset.csv')
@@ -17,13 +17,16 @@ def download_loan_data():
     except Exception as e:
         raise Exception(f"Veri yuklenirken bir hata olustu: {e}")
     
-    loan_df.to_csv(r'third_week_project/data/raw/loan_data.csv',index = False)
+    if save_to_disk:
+        base_dir = r"D:\Kairu_DS360_Projects\third_week_project"
+        os.makedirs(os.path.join(base_dir, "data/raw"), exist_ok=True)
+        loan_df.to_csv(os.path.join(base_dir, "data/raw/loan_data.csv"), index=False)
    
     return loan_df
 
-
-#%% main function (ana fonksiyon)
+loan_df = load_loan_data(save_to_disk=True)
+"""#%% main function (ana fonksiyon)
 if __name__ == "__main__":
-    download_loan_data()
+    load_loan_data(save_to_disk=True)
     
-        
+"""     
