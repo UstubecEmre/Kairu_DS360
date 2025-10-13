61d58ba17d31f888f026f939acce4bba26986066
UstubecEmre <emresb1999@gmail.com>
2025-10-13 12:20:33 +0300

    feat(fraud_detection_pipeline): add data validation for required columns, class values, and amount range

--- fraud_detection/src/fraud_detection_pipeline.py
+++ fraud_detection/src/fraud_detection_pipeline.py
@@ -1,5 +1,5 @@
 #%% import required libraries
-
+import os 
 import sys
 import json 
 import yaml 
@@ -8,6 +8,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+import kagglehub 
 
 from sklearn.model_selection import train_test_split, cross_val_score, StratifiedKFold #for cross validation
 from sklearn.ensemble import RandomForestClassifier, IsolationForest
@@ -125,4 +126,89 @@ class FraudDetectionPipeline():
         if mlflow.config.get('autolog', {}).get('sklearn', True):
             mlflow.sklearn.autolog()
         
-        logger.info(f"MLFlow Yapilandirilmasi Basarili Bir Sekilde Gerceklesti: {experiment_name}")                    
+        logger.info(f"MLFlow Yapilandirilmasi Basarili Bir Sekilde Gerceklesti: {experiment_name}")
+        
+        
+    def load_fraud_data_from_kagglehub(self, synthetic = True, data_path = None, dowload_with_kagglehub = False):
+        
+        """download_with_kagglehub argumani True ise kagglehub uzerinden fraud detection veri setini yukler"""
+        if dowload_with_kagglehub:
+            logger.info("KaggleHub Uzerinden Dolandiricilik Tespiti (Fraud Detection) Veri Seti Indiriliyor")
+            
+            try:
+                import kagglehub
+                path = kagglehub.dataset_download("mlg-ulb/creditcardfraud")
+                fraud_csv = os.path.join(path, 'creditcard.csv')
+                
+                # dosya var mi? Varsa uzerine yaz, yoksa olustur
+                if os.path.exists(fraud_csv):
+                    fraud_data = pd.read_csv(fraud_csv)
+                    logger.info(f"KaggleHub Uzerinden Veri Seti Indirildi. Veri Seti Boyutu: {fraud_data.shape}")  
+                else:
+                    logger.warning(f"Veri Seti KaggleHub Uzerinden Indirilemedi. Bilgisayariniza Indirebilirsiniz:")
+            except Exception as err:
+                logger.error(f"KaggleHub Uzerinden Indirme Basarisiz: {err}")  
+                #Dileyen yapay veri ureterek bu islemleri gerceklestirebilir, Program askida kalmaz.
+        else:
+            try:
+                fraud_data = pd.read_csv(data_path)
+                logger.info(f"Local'deki Yol Uzerinden Veri Seti Indiriliyor")
+            except FileNotFoundError:
+                logger.error("Dosya Yolu Bulunamadi. Lutfen Dosya Yolunu Kontrol Ediniz!!!")
+            except Exception as err:
+                logger.error(f"Beklenmeyen Bir Hata Olustu: {err}")
+        
+        # veri setini incele
+        self._validate_data(fraud_data)
+        
+        
+        # Veri setini X ve y olarak ayiralim (split)
+        X = fraud_data.drop('Class', axis= 1) # hedef degiskeni cikar
+        y = fraud_data['Class']
+        
+        
+        test_size = self.config['fraud_data']['test_size']
+        random_state = self.config['fraud_data']['random_state']
+        
+        # train test split
+        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(
+            X,
+            y,
+            test_size= test_size,
+            random_state=random_state,
+            stratify = y if self.config['fraud_data'].get('stratify', True) else None 
+        )    
+
+        logger.info(f"Veri Seti Yuklendi. Egitim Seti Boyutu: {len(self.X_train)}\nTest Seti Boyutu: {len(self.X_test)}")
+        logger.info(f"Sinif Dagilimlari:\n Egitim Seti Orani: {np.bincount(self.y_train)}\nTest Seti: {np.bincount(self.y_test)}")
+        
+    
+    def _validate_data(self, data):
+        """Veri setinin kalitesini ve dogrulunu test eder"""
+        validation_config = self.config.get('data', {}).get('validation',{})
+        
+        # Gerekli sutunlari alalim. Islem tutari, dolandiricilik sinifi, zaman
+        required_cols = validation_config.get('required_columns', ['Amount', 'Time','Class'])
+        missing_cols = [col for col in required_cols if col not in data.columns]
+        if missing_cols:
+            logger.error(f"Eksik Veri Bulunmaktadir: {missing_cols}")
+        
+        
+        # hedef degiskeni sayisal hale getir
+        if 'Class' in data.columns:
+            valid_classes = validation_config.get('class_values', [0, 1])
+            invalid_classes = data['Class'].unique() # tekil degerleri
+            invalid_classes = [col for col in invalid_classes if col not in valid_classes]
+            
+            if invalid_classes:
+                logger.error(f"Gecersiz Sinif Bulunmaktadir: {invalid_classes}")
+        
+        
+        # Amount degiskenini incele => 0 ile 1 Milyon araliginda tutalim
+        if 'Amount' in data.columns:
+            min_amount = validation_config.get('amount_min', 0)
+            max_amount = validation_config.get('amount_max', 1000000)
+            
+            if data['Amount'].min() < min_amount or data['Amount'].max() > max_amount:
+                logger.error(f"Beklenen Degerler Icerisinde Yer Almamaktadir. Beklenen Aralik: [{min_amount}, {max_amount}]")
+                
